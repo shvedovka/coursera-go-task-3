c04f2b3b6025859700a95c7e338e17b3e2f3ad75
Konstantin <kshvedov@gotrg.com>
2019-01-15 11:10:35 +0300

    improved x10

--- fast.go
+++ fast.go
@@ -1,10 +1,85 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
+	"os"
+	"strings"
 )
 
-// вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	seenBrowsers := make(map[string]bool)
+	uniqueBrowsers := 0
+	var foundUsers []string
+
+	var i = -1
+	//line := make([]byte, 0, 1024)
+	for {
+		line, err := reader.ReadSlice('\n')
+		if err != nil {
+			break
+		}
+
+		user := make(map[string]interface{})
+		err = json.Unmarshal(line, &user)
+
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			// log.Println("cant cast browsers")
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				// log.Println("cant cast browser to string")
+				continue
+			}
+
+			curIsAndroid := strings.Contains(browser, "Android")
+			curIsMSIE := strings.Contains(browser, "MSIE")
+			isMSIE = isMSIE || curIsMSIE
+			isAndroid = isAndroid || curIsAndroid
+
+			if _, found := seenBrowsers[browser]; found == true {
+				continue
+			}
+
+			if curIsAndroid || curIsMSIE {
+				seenBrowsers[browser] = true
+				uniqueBrowsers++
+			}
+			isMSIE = isMSIE || curIsMSIE
+			isAndroid = isAndroid || curIsAndroid
+
+		}
+		i++
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := strings.Replace(user["email"].(string), "@", " [at] ", 1)
+		foundUsers = append(foundUsers, fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email))
+	}
+
+	fmt.Fprintln(out, "found users:\n"+strings.Join(foundUsers, ""))
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
